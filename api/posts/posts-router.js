2cbac3eafaf189339b381a57645397989c029f46
GooieJS <conradklek@gmail.com>
2022-04-13 10:50:16 -0500

    submit

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -45,41 +45,36 @@ router.post('/', (req, res) => {
     }
 });
 
-router.delete('/:id', (req, res) => {
-    Post.remove(req.params.id)
-        .then(post => {
-            if (post) {
-                return Post.findById(post.id);
-            } else {
-                res.status(404).json({ message: 'The post with the specified ID does not exist.' });
-            }
-        })
-        .catch(error => {
-            res.status(500).json({ error: 'The post could not be removed.' });
-        });
+router.delete('/:id', async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if (!post) {
+            res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+        } else {
+            await Post.remove(req.params.id);
+            res.json(post)
+        }
+    } catch (err) {
+        res.status(500).json({ error: 'The post could not be removed.', err: err.message, stack: err.stack });
+    }
 });
 
 router.put('/:id', (req, res) => {
-    const { title, content } = req.body;
-    if (!title || !content) {
+    const { title, contents } = req.body;
+    if (!title || !contents) {
         res.status(400).json({ message: 'Please provide both a title and some content for the post.' });
     } else {
         Post.findById(req.params.id)
             .then(post => {
-                if (post) {
-                    post.title = title;
-                    post.content = content;
-                    return post.save();
-                } else {
+                if (!post) {
                     res.status(404).json({ message: 'The post with the specified ID does not exist.' });
+                } else {
+                    return Post.update(req.params.id, { title, contents })
                 }
             })
-            .then(post => {
-                if (post) res.status(200).json(post);
+            .then((post) => {
+                if (post) res.json(post)
             })
-            .catch(error => {
-                res.status(500).json({ error: 'The post information could not be updated.' });
-            });
     }
 });
 
